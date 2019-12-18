98a7f529b6bad6dd15bf45bdfb0fc14a6d823b43
Victor Gunnarsson <victor@educar.se>
2019-12-18 17:07:15 +0100

    Updates

---
+++ precache-manifest.eb3d64a93fbc5eef3f2ac86e822b7c74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37895aa535cae4534c62f9ed615a5689",
+    "url": "/eslovsemb/index.html"
+  },
+  {
+    "revision": "63eb213ef8cc9a22df7b",
+    "url": "/eslovsemb/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "285c583367ea6cf0745c",
+    "url": "/eslovsemb/static/css/main.2d4aee3e.chunk.css"
+  },
+  {
+    "revision": "63eb213ef8cc9a22df7b",
+    "url": "/eslovsemb/static/js/2.b2f1a110.chunk.js"
+  },
+  {
+    "revision": "285c583367ea6cf0745c",
+    "url": "/eslovsemb/static/js/main.e3d639b2.chunk.js"
+  },
+  {
+    "revision": "4d4af04a7a4679bb3db4",
+    "url": "/eslovsemb/static/js/runtime~main.5ee02f9c.js"
+  },
+  {
+    "revision": "3f607850ff83e7fbe8eb364984e0cce0",
+    "url": "/eslovsemb/static/media/Carrousel1.3f607850.jpg"
+  },
+  {
+    "revision": "0aaed7c9e1e94fc4ced77d4fbb798352",
+    "url": "/eslovsemb/static/media/Carrousel2.0aaed7c9.jpg"
+  },
+  {
+    "revision": "eaaa0655b50fc320f18d15438bdf196c",
+    "url": "/eslovsemb/static/media/Carrousel3.eaaa0655.jpg"
+  },
+  {
+    "revision": "8842192d3691610c56b874e0a5fc9a68",
+    "url": "/eslovsemb/static/media/landing-wood.8842192d.jpg"
+  }
+]);
